8096d0b72acb207739c31ead0cbd2616f498cc24
ZahraAbdolalian <abdolalianzahra@gmail.com>
2026-05-29 23:13:09 +0330

    add main components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppShell } from '@core/layout/app-shell/app-shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppShell,
+        pathMatch: 'full',
+        loadComponent: () => import('@features/home/home').then(m => m.Home)
+    }
+];
