9783cea402e44f588b94db797f1ceff7ef4d537f
Zahra Abdolalian <abdolalianzahra@gmail.com>
2026-06-09 15:31:02 +0330

    use custom card in sidebar

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { NcsCard } from '../../shared/ncs-card/ncs-card';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatCardModule],
+  imports: [MatCardModule, NcsCard],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
